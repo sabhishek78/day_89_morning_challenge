f73db079c4d6fe917c85bb6c999ef3b00e656d54
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-02-27 10:44:26 +0530

    finished

--- bin/main.dart
+++ bin/main.dart
@@ -2,6 +2,34 @@
 // The output should be a sorted list as well.
 
 void main() {
- print('HEY');
+ print(mergeLists([1,2], [1,2,3]));
 }
+List<int> mergeLists(List<int>l1,List<int>l2){
+ List<int> result =List<int>.filled((l2.length-l1.length).abs()+l1.length>l2.length?l2.length:l1.length, 0, growable: true);
+ if(l1.length>l2.length){
+  int i;
+  for( i=0;i<l2.length;i++){
+    result[i]=l1[i]+l2[i];
+  }
+  for(i=l2.length;i<l1.length;i++){
+   result.add(l1[i]);
+  }
+ }
+ else if(l1.length<l2.length){
+  int i;
+  for( i=0;i<l1.length;i++){
+   result[i]=l1[i]+l2[i];
+  }
+  for(i=l1.length;i<l2.length;i++){
+   result.add(l2[i]);
+  }
+ }else if(l1.length==l2.length){
+  int i;
+  for( i=0;i<l1.length;i++){
+   result[i]=l1[i]+l2[i];
+  }
 
+ }
+ result.sort();
+ return result;
+}

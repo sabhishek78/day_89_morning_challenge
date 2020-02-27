ca2a482fcb1274bb6d6e9faefb134185e4e2541d
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2020-02-27 11:09:08 +0530

    code for merging corrected.

--- bin/main.dart
+++ bin/main.dart
@@ -3,33 +3,18 @@
 
 void main() {
  print(mergeLists([1,2], [1,2,3]));
+ print(mergeLists([1,2], [3]));
+ print(mergeLists([], [3]));
 }
 List<int> mergeLists(List<int>l1,List<int>l2){
- List<int> result =List<int>.filled((l2.length-l1.length).abs()+l1.length>l2.length?l2.length:l1.length, 0, growable: true);
- if(l1.length>l2.length){
-  int i;
-  for( i=0;i<l2.length;i++){
-    result[i]=l1[i]+l2[i];
-  }
-  for(i=l2.length;i<l1.length;i++){
-   result.add(l1[i]);
-  }
- }
- else if(l1.length<l2.length){
-  int i;
-  for( i=0;i<l1.length;i++){
-   result[i]=l1[i]+l2[i];
-  }
-  for(i=l1.length;i<l2.length;i++){
-   result.add(l2[i]);
-  }
- }else if(l1.length==l2.length){
-  int i;
-  for( i=0;i<l1.length;i++){
-   result[i]=l1[i]+l2[i];
+ int i;
+ for(i=0;i<l1.length;i++){
+     int j=0;
+    while(l2[j]<=l1[i]){
+     j++;
+    }
+    l2.insert(j, l1[i]);
   }
 
- }
- result.sort();
- return result;
+return l2;
 }
